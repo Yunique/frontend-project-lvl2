5d53a5fcd7d65890386e5a65f159be07c4f01691
Yunik Stepanyan <yurastep92@gmail.com>
2020-02-03 12:18:41 +0400

    still making it work

--- makefile
+++ makefile
@@ -4,5 +4,8 @@ install:
 publish:
 	npm publish --dry-run
 
+start:
+	npx babel-node src/bin/gendiff
+
 lint: 
 	npx eslint .
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,6 +1,4 @@
-#!/usr/bin/env node.
-
-const program = require('commander');
+export const program = require('commander');
 
 program
     .version('0.0.1', '-v, --vers', 'output the version number')
