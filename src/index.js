869f9b65c32bff2ff6a1771d18680d391a547aec
Yunik Stepanyan <yurastep92@gmail.com>
2020-02-08 23:22:20 +0400

    make it work correct

--- src/index.js
+++ src/index.js
@@ -5,19 +5,24 @@ const getFile = (path) => fs.readFileSync(path);
 
 const fileToObj = (file) => JSON.parse(file);
 
-const compareObjects = (firstColl, secondColl) => _.reduce(
-  firstColl, (result, value, key) => {
-    if (_.has(secondColl, key)) {
-      return (value === secondColl[key])
-        ? [...result, `${key}: ${value}`]
-        : [...result, `+ ${key}: ${value}`, `+ ${key}: ${secondColl[key]}`];
-    }
-    return [...result, `- ${key}: ${value}`];
-  }, [],
-);
-
+const compareColls = (firstColl, secondColl) => {
+  const keys = _.uniq([..._.keys(firstColl), ..._.keys(secondColl)]);
+  return _.reduce(
+    keys, (result, key) => {
+      if (!_.has(firstColl, key)) {
+        return [...result, `  + ${key}: ${secondColl[key]}`];
+      }
+      if (_.has(secondColl, key)) {
+        return (firstColl[key] === secondColl[key])
+          ? [...result, `    ${key}: ${secondColl[key]}`]
+          : [...result, `  + ${key}: ${firstColl[key]}`, `  + ${key}: ${secondColl[key]}`];
+      }
+      return [...result, `  - ${key}: ${firstColl[key]}`];
+    }, [],
+  );
+};
 const getDifference = (path1, path2) => {
-  const comparsion = compareObjects(fileToObj(getFile(path1)), fileToObj(getFile(path2)));
+  const comparsion = compareColls(fileToObj(getFile(path1)), fileToObj(getFile(path2)));
   return comparsion.join('\n');
 };
 
