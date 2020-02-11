b7382c475d58f34bd7eff1cc87d10a3e7ad3500a
Yunik Stepanyan <yurastep92@gmail.com>
2020-02-11 13:44:12 +0400

    refactor comparsion module and rendering

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import parser from './utils/parsers';
 import compareColls from './utils/compareColls';
 
 const getDifference = (path1, path2) => {
-  const comparsion = compareColls(parser(path1), parser(path2)).join('\n');
-  const result = `{\n${comparsion}\n}`;
+  const comparsion = compareColls(parser(path1), parser(path2));
+  const mapped = comparsion.map(([key, [value, cond]]) => `  ${cond} ${key}: ${value}`);
+  const result = `{\n${mapped.join('\n')}\n}`;
   return result;
 };
 
--- src/utils/compareColls.js
+++ src/utils/compareColls.js
@@ -5,14 +5,14 @@ export default (firstColl, secondColl) => {
   return _.reduce(
     keys, (result, key) => {
       if (!_.has(firstColl, key)) {
-        return [...result, `  + ${key}: ${secondColl[key]}`];
+        return [...result, [key, [secondColl[key], '+']]];
       }
       if (_.has(secondColl, key)) {
         return (firstColl[key] === secondColl[key])
-          ? [...result, `    ${key}: ${secondColl[key]}`]
-          : [...result, `  - ${key}: ${firstColl[key]}`, `  + ${key}: ${secondColl[key]}`];
+          ? [...result, [key, [secondColl[key], ' ']]]
+          : [...result, [key, [firstColl[key], '-']], [key, [secondColl[key], '+']]];
       }
-      return [...result, `  - ${key}: ${firstColl[key]}`];
+      return [...result, [key, [firstColl[key], '-']]];
     }, [],
   );
 };
