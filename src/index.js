aecedbd73eb28b8938da72d96cb20343ab2a6f13
Yunik Stepanyan <yurastep92@gmail.com>
2020-02-15 17:51:03 +0400

    for refactor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+import _ from 'lodash';
 import parser from './utils/parsers';
 import compareColls from './utils/compareColls';
 
+
+const stringify = (value) => `${value.slice(0)}`;
+
 const getDifference = (path1, path2) => {
+  const iter = (item, acc) => {
+    if (_.isObject(item)) {
+      return _.map(item, (subItem) => iter(subItem));
+    }
+    return stringify(item);
+  };
+
+
   const comparsion = compareColls(parser(path1), parser(path2));
-  const mapped = comparsion.map(([key, [value, cond]]) => `  ${cond} ${key}: ${value}`);
+  console.log(comparsion);
+  const mapped = comparsion.map(({keyj}) => `  ${cond} ${key}: ${value}`);
   const result = `{\n${mapped.join('\n')}\n}`;
   return result;
 };
--- src/utils/compareColls.js
+++ src/utils/compareColls.js
@@ -1,18 +1,41 @@
 import _ from 'lodash';
 
-export default (firstColl, secondColl) => {
+const makeElem = (key, value, cond = ' ') => ({ key, value, cond });
+
+const getType = (key, firstColl, secondColl) => {
+  if (!_.has(firstColl, key)) {
+    return makeElem(key, secondColl[key], '+');
+  }
+  if (_.has(secondColl, key)) {
+    return (firstColl[key] === secondColl[key])
+      ? makeElem(key, secondColl[key], ' ')
+      : [makeElem(key, firstColl[key], '-'), makeElem(key, secondColl[key], '+')];
+  }
+  return makeElem(key, firstColl[key], '-');
+};
+
+const genDiff = (firstColl, secondColl) => {
   const keys = _.uniq([..._.keys(firstColl), ..._.keys(secondColl)]);
+
   return _.reduce(
-    keys, (result, key) => {
-      if (!_.has(firstColl, key)) {
-        return [...result, [key, [secondColl[key], '+']]];
+    keys, (acc, key) => {
+      if (_.isObject(firstColl[key]) && _.isObject(secondColl[key])) {
+        return [...acc, makeElem(key, genDiff(firstColl[key], secondColl[key])), ' '];
       }
-      if (_.has(secondColl, key)) {
-        return (firstColl[key] === secondColl[key])
-          ? [...result, [key, [secondColl[key], ' ']]]
-          : [...result, [key, [firstColl[key], '-']], [key, [secondColl[key], '+']]];
+      if (_.isObject(secondColl[key])) {
+        return (_.has(firstColl, key))
+          ? [...acc, makeElem(key, firstColl[key], '-'), makeElem(key, secondColl[key], '+')]
+          : [...acc, makeElem(key, secondColl[key], '+')];
       }
-      return [...result, [key, [firstColl[key], '-']]];
+      if (_.isObject(firstColl[key])) {
+        return (_.has(secondColl, key))
+          ? [...acc, makeElem(key, firstColl[key], '-'), makeElem(key, secondColl[key], '+')]
+          : [...acc, makeElem(key, firstColl[key], '-')];
+      }
+
+      return [...acc, getType(key, firstColl, secondColl)];
     }, [],
   );
 };
+
+export default genDiff;
