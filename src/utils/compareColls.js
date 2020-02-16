b3edfb3063e778c784e2882ff09d088f2f26d38c
Yunik Stepanyan <yurastep92@gmail.com>
2020-02-16 15:35:04 +0400

    refactor making AST

--- src/utils/compareColls.js
+++ src/utils/compareColls.js
@@ -1,41 +1,27 @@
 import _ from 'lodash';
 
-const makeElem = (key, value, cond = ' ') => ({ key, value, cond });
-
-const getType = (key, firstColl, secondColl) => {
-  if (!_.has(firstColl, key)) {
-    return makeElem(key, secondColl[key], '+');
-  }
-  if (_.has(secondColl, key)) {
-    return (firstColl[key] === secondColl[key])
-      ? makeElem(key, secondColl[key], ' ')
-      : [makeElem(key, firstColl[key], '-'), makeElem(key, secondColl[key], '+')];
-  }
-  return makeElem(key, firstColl[key], '-');
+const config = {
+  added: 'added',
+  changed: 'changed',
+  unchanged: 'unchanged',
+  deleted: 'deleted',
+  parent: 'parent',
 };
 
-const genDiff = (firstColl, secondColl) => {
+const makeAST = (firstColl, secondColl) => {
   const keys = _.uniq([..._.keys(firstColl), ..._.keys(secondColl)]);
 
   return _.reduce(
-    keys, (acc, key) => {
-      if (_.isObject(firstColl[key]) && _.isObject(secondColl[key])) {
-        return [...acc, makeElem(key, genDiff(firstColl[key], secondColl[key])), ' '];
-      }
-      if (_.isObject(secondColl[key])) {
-        return (_.has(firstColl, key))
-          ? [...acc, makeElem(key, firstColl[key], '-'), makeElem(key, secondColl[key], '+')]
-          : [...acc, makeElem(key, secondColl[key], '+')];
-      }
-      if (_.isObject(firstColl[key])) {
-        return (_.has(secondColl, key))
-          ? [...acc, makeElem(key, firstColl[key], '-'), makeElem(key, secondColl[key], '+')]
-          : [...acc, makeElem(key, firstColl[key], '-')];
-      }
-
-      return [...acc, getType(key, firstColl, secondColl)];
-    }, [],
+    keys, (acc, key) => [...acc, {
+      type: config,
+      nodeName: key,
+      oldValue: firstColl[key],
+      newValue: secondColl[key],
+      children: ((_.isObject(firstColl[key]) && _.isObject(secondColl[key]))
+        ? makeAST(firstColl[key], secondColl[key])
+        : null),
+    }], [],
   );
 };
 
-export default genDiff;
+export default makeAST;
