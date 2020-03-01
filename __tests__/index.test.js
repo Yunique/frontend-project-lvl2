974c1df51708756cf17f661d2690c44798ffd776
Yunik Stepanyan <yurastep92@gmail.com>
2020-03-01 11:33:37 +0300

    fix duplication in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,37 +2,23 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '../src';
 
-const getOriginalFilesPaths = (extension) => {
-  const beforeFilePath = path.join(__dirname, `/__fixtures__/beforeMulti.${extension}`);
-  const afterFilePath = path.join(__dirname, `/__fixtures__/afterMulti.${extension}`);
-  return { beforeFilePath, afterFilePath };
+const getTestDataPaths = (extension, format) => {
+  const beforeFilePath = path.join(__dirname, '/__fixtures__/', `before.${extension}`);
+  const afterFilePath = path.join(__dirname, '/__fixtures__/', `after.${extension}`);
+  const resultFilePath = path.join(__dirname, '/__fixtures__/', `result${format}`);
+  return { beforeFilePath, afterFilePath, resultFilePath };
 };
 
-const getResultFilePath = (format) => path.join(__dirname, `/__fixtures__/result${format}`);
-
 const extensions = ['json', 'yml', 'ini'];
+const formats = ['Operanded', 'Plain', 'JSON'];
 
 describe.each(extensions)(
   'generate difference between %s files',
   (extension) => {
-    const { beforeFilePath, afterFilePath } = getOriginalFilesPaths(extension);
-
-    test('operanded output', () => {
-      const result = getResultFilePath('Operanded');
-      expect(genDiff(beforeFilePath, afterFilePath, 'Operanded'))
-        .toBe(fs.readFileSync(result, 'utf8'));
-    });
-
-    test('plain output', () => {
-      const result = getResultFilePath('Plain');
-      expect(genDiff(beforeFilePath, afterFilePath, 'Plain'))
-        .toBe(fs.readFileSync(result, 'utf8'));
-    });
-
-    test('JSON output', () => {
-      const result = getResultFilePath('JSON');
-      expect(genDiff(beforeFilePath, afterFilePath, 'JSON'))
-        .toBe(fs.readFileSync(result, 'utf8'));
+    test.each(formats)('%s output', (format) => {
+      const { beforeFilePath, afterFilePath, resultFilePath } = getTestDataPaths(extension, format);
+      expect(genDiff(beforeFilePath, afterFilePath, format))
+        .toBe(fs.readFileSync(resultFilePath, 'utf8'));
     });
   },
 );
